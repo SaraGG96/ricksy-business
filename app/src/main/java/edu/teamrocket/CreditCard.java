337f4902f030f9e0c2343c0e4cfcd5f5e7f42197
SaraGG96 <saragarciagarrido96@gmail.com>
2026-05-02 11:35:34 +0200

    feat(CreditCard): Añadir clase CreditCard

---
+++ app/src/main/java/edu/teamrocket/CreditCard.java
@@ -0,0 +1,15 @@
+package edu.teamrocket;
+
+public class CreditCard {
+
+    private String owner;
+    private String number;
+    private int credit = 3000;
+    private static String SYMBOL;
+
+    public CreditCard(String owner, String number) {
+        this.owner = owner;
+        this.number = number;
+    }
+    
+}
