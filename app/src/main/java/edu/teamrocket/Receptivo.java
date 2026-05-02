3c6fc5368b610c6b17b9e4d425d89f02b9b88aa4
SaraGG96 <saragarciagarrido96@gmail.com>
2026-05-02 16:02:38 +0200

    feat(Receptivo): Implementar clase Receptivo

---
+++ app/src/main/java/edu/teamrocket/Receptivo.java
@@ -0,0 +1,10 @@
+package edu.teamrocket;
+
+public class Receptivo implements GuestDispatcher {
+
+    public Receptivo() {}
+
+    public void registra(GuestDispatcher dispatcher) {}
+    public void dispatch(CreditCard card) {}
+    
+}
