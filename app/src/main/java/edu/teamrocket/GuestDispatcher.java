1468e4fcc022e24beaaefed8abfbcb26f91939ed
SaraGG96 <saragarciagarrido96@gmail.com>
2026-05-02 18:32:13 +0200

    refactor: Eliminar registra

--- app/src/main/java/edu/teamrocket/GuestDispatcher.java
+++ app/src/main/java/edu/teamrocket/GuestDispatcher.java
@@ -1,8 +1,5 @@
 package edu.teamrocket;
 
 public interface GuestDispatcher {
-
-    void dispatch(CreditCard card);
-    void registra(GuestDispatcher dispatcher);
-    
+    void dispatch(CreditCard card);    
 }
