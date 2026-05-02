742cfb742f7bcddfe3c8ee8e722187f5212b5752
SaraGG96 <saragarciagarrido96@gmail.com>
2026-05-02 12:01:30 +0200

    feat(UfosPark): HU3: Dar de alta en la flota

--- app/src/main/java/edu/teamrocket/UfosPark.java
+++ app/src/main/java/edu/teamrocket/UfosPark.java
@@ -3,6 +3,8 @@ package edu.teamrocket;
 public class UfosPark {
     private int fee = 500;
 
+    public void add(String ufoID) {}
+
     public void dispatch(CreditCard card) {
         if (card.getCredit() >= fee) {
             card.setCredit(card.getCredit() - fee);
