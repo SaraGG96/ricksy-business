d61f3020c0087cad9a75007533c510bcc33be5e1
SaraGG96 <saragarciagarrido96@gmail.com>
2026-05-02 15:55:45 +0200

    feat(CrystalExpender): HU5: Implementar CrystalExpender

---
+++ app/src/main/java/edu/teamrocket/CrystalExpender.java
@@ -0,0 +1,13 @@
+package edu.teamrocket;
+
+public class CrystalExpender {
+
+    private int stock;
+    private double itemCost;
+
+    public CrystalExpender(int stock, double itemCost) {
+        this.stock = stock;
+        this.itemCost = itemCost;
+    }
+
+}
