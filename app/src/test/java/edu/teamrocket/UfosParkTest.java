d8580d2266f040428012ad10eb47a4ec61d9791e
SaraGG96 <saragarciagarrido96@gmail.com>
2026-05-04 12:33:18 +0200

    test: Añadir caso test para UfosPark

---
+++ app/src/test/java/edu/teamrocket/UfosParkTest.java
@@ -0,0 +1,31 @@
+package edu.teamrocket;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+public class UfosParkTest {
+    
+    UfosPark parktest = new UfosPark();
+
+    @Test
+    public void addTest() {
+        parktest.add("ovni1");
+        parktest.add("ovni2");
+        parktest.add("ovni3");
+        assert(parktest.toString().equals("UfosPark con 3 ufos alquilados."));
+    }
+
+    @Test
+    public void dispatchTest() {
+        CreditCard card1 = new CreditCard("Ricky", "1234567890123456");
+        parktest.add("ovni1");
+        parktest.dispatch(card1);
+        assertEquals("ovni1", parktest.getUfoOf("1234567890123456"));
+    }
+
+
+
+}
